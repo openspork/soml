9f95bf9858470066db408283543fd97e23745dbb
Christian Gerlach <christianrgerlach@gmail.com>
2018-02-17 20:08:32 -0800

    full path to images

--- soml/config.py
+++ soml/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    UPLOADED_SHITPICS_DEST = '../data/images/shitpics/'
+    UPLOADED_SHITPICS_DEST = 'FULLPATH/data/images/shitpics/'
     UPLOADED_SHITPICS_URL = 'shitpic/get/'
     
